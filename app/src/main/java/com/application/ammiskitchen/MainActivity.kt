7948eb0ab9e35cc924076fb63cf5927088b269f7
KartikCD <chawdakartik@gmail.com>
2021-10-21 01:44:36 +0530

    feat: login and mobile verification ui done.
    Signed-off-by: KartikCD <chawdakartik@gmail.com>

--- app/src/main/java/com/application/ammiskitchen/MainActivity.kt
+++ app/src/main/java/com/application/ammiskitchen/MainActivity.kt
@@ -2,10 +2,37 @@ package com.application.ammiskitchen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.application.ammiskitchen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.AppTheme)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.main_host_fragment)
+        val appBarConfiguration = AppBarConfiguration
+                .Builder(R.id.mainFeedFragment, R.id.loginFragment)
+                .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.main_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
